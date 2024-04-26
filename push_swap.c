86ad57314fa2e8e0c154355bf7d4a539cc98b75f
Graham Stronge <165650024+gstronge@users.noreply.github.com>
2024-04-26 11:22:28 +0200

    changed to linked list

--- push_swap.c
+++ push_swap.c
@@ -1,3 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gstronge <gstronge@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 10:37:59 by gstronge          #+#    #+#             */
+/*   Updated: 2024/04/26 11:21:38 by gstronge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 // =========== REMOVE ==================================================================================
 #include <stdio.h>
 // =========== REMOVE ==================================================================================
@@ -5,286 +18,191 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// =========== PUT IN HEADER FILE ======================================================================
+typedef	struct s_list
+{
+	int				data;
+	int				size; // am I using this? =======================================================
+	struct s_list	*next;
+}	t_list;
+// =========== PUT IN HEADER FILE ======================================================================
 
 // =========== REMOVE ==================================================================================
-
-void	ft_print_all(int *a, int *b, int argc)
-{
-	printf("=======\n");
-	int i = 0;
-	while (i < argc - 1)
+void	ft_print_both(t_list *a, t_list *b)
+{	
+	printf("\n=========\n");
+	while (a != NULL || b != NULL)
 	{
-		printf("%d %d\n", a[i], b[i]);
-		i++;
+		if (a != NULL)
+		{
+			printf("%d  ", a->data);
+			a = a->next;
+		}
+		else
+			printf("x  ");
+		if (b != NULL)
+		{
+			printf("%d\n", b->data);
+			b = b->next;
+		}
+		else
+			printf("x\n");
 	}
-	printf("==========================\n\n");
+	printf("======================================\n\n");
 }
 // =========== REMOVE ==================================================================================
 
-int	ft_atoi(char *str, int *a)
+int	ft_atoi(char *str)
 {
-	int num;
-	int negative;
-	int i;
+	long	num;
+	int		negative;
+	int		i;
 
 	num = 0;
 	negative = 1;
 	i = 0;
-	while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\t')) // add other whitespace symbols
-		i++;
-	if (str[i] == 0)
-		return (0);
-	if (str[i] == '-')
+	if (str[0] == '0')
+		return(0);
+	if (str[0] == '-')
 	{
 		negative = -1;
+		num *= -1;
 		i++;
 	}
 	while (str[i] != '\0')
 	{
-		if (str[i] > '9' || str[i] < '0')
-		{
-			write(1, "Error", 5);
-			free (a);
-			exit (0);
-		}
 		num = (num * 10) + str[i] - '0';
 		i++;
 	}
-	return(num * negative);
+	return (num * negative);
 }
 
-int	*ft_create_a(int *a, int size, char **argv)
+int	ft_add_node_end(t_list *current, int size, char *str)
 {
+	t_list	*new;
+
+	new = (t_list *)malloc(1 * sizeof(t_list));
+	current->next = new;
+	new->next = NULL;
+	new->data = ft_atoi(str);
+	return (size + 1);
+}
+
+t_list	*ft_create_a(t_list *a, int argc, char **argv)
+{
+	t_list	*current;
+	int size;
 	int i;
 
-	i = 0;
-	if (!(a = malloc(size * sizeof(int))))
-		return (0);
-	while (i < size)
+	a = (t_list *)malloc(1 * sizeof(t_list));
+	if (a == NULL)
+		return(NULL);
+	size = 1;
+	i = 1;
+	a->data = ft_atoi(&argv[i][0]);
+	a->next = NULL;
+	i++;
+	current = a;
+	while (i < argc)
 	{
-		a[i] = ft_atoi(argv[i + 1], a);
+		size = ft_add_node_end(current, size, &argv[i][0]);
+		current = current->next;
 		i++;
 	}
 	return (a);
 }
 
-int ft_check_order(int *a, int argc, int size)
+t_list	*ft_rotate(t_list *list, char c)
 {
-	int i;
+	t_list	*head;
+	t_list	*tail;
 
-	i = 0;
-	int t = argc;
-	t++;
-	// if (size + 1 != argc)
-	// 	return (1);
-	
-	while (i < (size - 1))
-	{
-		if (a[i] > a[i + 1])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (list == NULL || list->next == NULL)
+		return (NULL);
+	tail = list;
+	head = list->next;
+	while (list->next != NULL)
+		list = list->next;
+	list->next = tail;
+	tail->next = NULL;
+	write(1, "r", 1);
+	write(1, &c, 1);
+	return (head);
 }
 
-void	ft_ra(int *a, int size, int *b, int argc) // ================== REMOVE , int *b, int argc ========== REMOVE b and argc ==========
+t_list	*ft_swap(t_list *list, char c)
 {
-	int temp;
-	int i;
+	int		temp;
 
-	temp = a[0];
-	i = 0;
-	while (i < size - 1)
-	{
-		a[i] = a[i + 1];
-		i++;
-	}
-	a[size - 1] = temp;
-	write(1, "ra\n", 3);
-	ft_print_all(a, b, argc);// ================== REMOVE ========== REMOVE ========== REMOVE ========== REMOVE ==================== 
+	if (list == NULL || list->next == NULL)
+		return (NULL);
+	temp = list->data;
+	list->data = list->next->data;
+	list->next->data = temp;
+	write(1, "s", 1);
+	write(1, &c, 1);
+	return (list);
 }
 
-void	ft_sa(int *a, int *b, int argc) // ================== REMOVE , int *b, int argc ========== REMOVE b and argc ========== 
-{
-	int temp;
 
-	temp = a[0];
-	a[0] = a[1];
-	a[1] = temp; 
-	write(1, "sa\n", 3);
-	ft_print_all(a, b, argc);// ================== REMOVE ========== REMOVE ========== REMOVE ========== REMOVE ==================== 
+t_list	*ft_push_to(t_list **from, t_list **to, char c)
+{
+	t_list	*pushed_node;
+
+	if (*from == NULL)
+		return (*to);
+	pushed_node = *from;
+	*from = (*from)->next;
+	pushed_node->next = *to;
+	write(1, "p", 1);
+	write(1, &c, 1);
+	return (pushed_node);
 }
 
-int	ft_largest_num(int *a, int size, int largest_num)
+t_list	*ft_rev_rotate(t_list *list, char c)
 {
-	int i;
+	t_list	*head;
+	t_list	*tail;
 
-	i = 0;
-	largest_num = -2147483648;
-	while (i < size)
-	{
-		if (a[i] > largest_num)
-			largest_num = a[i];
-		i++;
-	}
-	return (largest_num);
+	if (list == NULL || list->next == NULL)
+		return (NULL);
+	tail = list;
+	while (tail->next->next != NULL)
+		tail = tail->next;
+	head = tail->next;
+	head->next = list;
+	tail->next = NULL;
+	write(1, "rr", 2);
+	write(1, &c, 1);
+	return (head);
 }
-int	ft_smallest_num(int *a, int size, int smallest_num)
-{
-	int i;
-
-	i = 0;
-	smallest_num = 2147483647;
-	while (i < size)
-	{
-		if (a[i] < smallest_num)
-			smallest_num = a[i];
-		i++;
-	}
-	return (smallest_num);
-}
-
-int	ft_find_next(int *a, int size, int largest_num, int next_num)
-{
-	int i;
-
-	next_num = largest_num - 1;
-	i = 0;
-	while (a[i] != next_num)
-	{
-		while (i < size)
-		{
-			if (a[i] == next_num)
-				return (next_num);
-			i++;
-		}
-		i = 0;
-		next_num--;
-	}
-	return (next_num);
-}
-
-int	ft_pb(int *a, int *b, int size, int argc)
-{
-	int i;
-	int sizeb;
-
-	sizeb = argc - 1 - size;
-	size--;
-	i = 0;
-	while (sizeb > 0)
-	{
-		b[sizeb] = b[sizeb - 1];
-		sizeb--;
-	}
-	b[0] = a[0];
-	while (i < size)
-	{
-		a[i] = a[i + 1];
-		i++;
-	}
-	write(1, "pb\n", 3);
-	ft_print_all(a, b, argc);// ================== REMOVE ========== REMOVE ========== REMOVE ========== REMOVE ==================== 
-	return(size);
-}
-
-int	ft_pa(int *a, int *b, int size, int argc)
-{
-	int i;
-	int sizeb;
-
-	i = size;
-	size++;
-	sizeb = argc - 1 - size;
-	while (i > 0)
-	{
-		a[i] = a[i - 1];
-		i--;
-	}
-	a[0] = b[0];
-	while (i < sizeb)
-	{
-		b[i] = b[i + 1];
-		i++;
-	}
-	write(1, "pa\n", 3);
-	ft_print_all(a, b, argc);// ================== REMOVE ========== REMOVE ========== REMOVE ========== REMOVE ====================
-	return(size);
-}
-
 int main(int argc, char **argv)
 {
-	int *a;
-	int *b;
-	int size;
-	int largest_num;
-	int smallest_num;
-	int	next_num;
+	t_list	*a;
+	t_list	*b;
+	int counter = 0;// ============ REMOVE =================== REMOVE =================== REMOVE =========================
 
 	a = NULL;
-	size = argc - 1;
-	largest_num = -2147483648;
-	smallest_num = 2147483647;
-	next_num = 0;
+	b = NULL;
 	if (argc > 1)
 	{
-		a = ft_create_a(a, size, argv);
-		if (!(b = malloc(size * sizeof(int))))
-			return (0);
+		a = ft_create_a(a, argc, argv);
 	}
-	largest_num = ft_largest_num(a, size, largest_num);
-	smallest_num = ft_smallest_num(a, size, smallest_num);
 
 // ============ REMOVE =================== REMOVE ================= REMOVE =========================
-	int i = 0;
-	printf("\n");
-	while (i < size)
-	{
-		printf("%d, ", a[i]);
-		i++;
-	}
-	printf("\n\n");
-// ============ REMOVE =================== REMOVE =================== REMOVE =========================
+	ft_print_both(a, b);
+// // ============ REMOVE =================== REMOVE =================== REMOVE ====================
 
-	int counter = 0;
+		b = ft_push_to(&a, &b, 'b');
 
-	while (!ft_check_order(a, argc, size))
-	{
-		if (a[0] > a[1])
-		{
-			ft_sa(a, b, argc);
-			counter++;
-		}
-		else if (a[0] == smallest_num)
-		{
-			size = ft_pb(a, b, size, argc);
-			smallest_num = ft_smallest_num(a, size, smallest_num);
-			counter++;
-		}
-		else
-		{
-			ft_ra(a, size, b , argc);
-			counter++;
-		}
-	}
-	while (size < argc - 1)
-	{
-		size = ft_pa(a, b, size, argc);
-		counter++;
-	}
-	
-	printf("\n%d\n", counter);
+// ============ REMOVE =================== REMOVE ================= REMOVE =========================
+	ft_print_both(a, b);
+// // ============ REMOVE =================== REMOVE =================== REMOVE =========================
 
-// ============ REMOVE =================== REMOVE =================== REMOVE =========================
-	i = 0;
-	while (i < size)
-	{
-		printf("%d, ", a[i]);
-		i++;
-	}
-	printf("\n");
-// ============ REMOVE =================== REMOVE =================== REMOVE =========================
 
-	free(a);
-	free(b);
+	printf("\n%d\n", counter);// ============ REMOVE =================== REMOVE =================== REMOVE ========================= 
+
+
+	ft_free(a, b);
+	// free(b);
 }
